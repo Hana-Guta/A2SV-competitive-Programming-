69cd46db30ca74152f068f4e4868551f4a8bb7b1
Hana <120221238+Hana-Guta@users.noreply.github.com>
2023-07-22 14:49:52 +0300

    Create ReplaceElements.py

---
+++ ReplaceElements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
+        dic={}
+        for idx,i in enumerate(nums):
+            dic[i]=idx
+        for i,j in operations:
+            nums[dic[i]]=j
+            dic[j]=dic[i]
+        return nums
+        
