8e1933d2e75da019382b6ea1472dd3d6cdd7805a
Hana <120221238+Hana-Guta@users.noreply.github.com>
2023-08-25 14:01:16 +0300

    Update subarraysum.py

--- subarraysum.py
+++ subarraysum.py
@@ -6,13 +6,10 @@ class Solution:
         
         for num in nums:
             prefix_sum += num
-            
-            # Check if there is a prefix sum such that prefix_sum - cumulative_sum = k
-            # This means the subarray between that prefix sum and the current index has a sum of k
+    
             if prefix_sum - k in prefix_sum_count:
                 count += prefix_sum_count[prefix_sum - k]
-            
-            # Update the count of the current prefix sum in the dictionary
+        
             prefix_sum_count[prefix_sum] = prefix_sum_count.get(prefix_sum, 0) + 1
         
         return count
