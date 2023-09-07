fd9dd5c8a331f8fa8649641363400e3938a09c1f
Hana <120221238+Hana-Guta@users.noreply.github.com>
2023-09-07 18:50:56 +0300

    Create Parenthesis_Score.py

---
+++ Parenthesis_Score.py
@@ -0,0 +1,21 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack = []
+        score = 0
+
+        for char in s:
+            if char == '(':
+                stack.append(score)
+                score = 0
+            else:
+                last_score = stack.pop()
+                score = last_score + max(2 * score, 1)
+
+        return score
+
+
+
+
+
+        
+        
