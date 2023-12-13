0a1f178c4925d49472b2d744158ceb4514769791
Hana <120221238+Hana-Guta@users.noreply.github.com>
2023-12-13 19:32:10 +0300

    Added solution for Minimize String Length

---
+++ onboarding/minimize-string-length.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution:
+    def minimizedStringLength(self, s: str) -> int:
+        count = Counter(s)
+        
+        for key, value in count.items():
+            if value > 1 :
+                count[key] = 1
+
+        return sum(count.values())
+
