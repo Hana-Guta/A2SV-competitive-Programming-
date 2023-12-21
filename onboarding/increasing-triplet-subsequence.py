021f8a3c01a1245660ec72f3cb22f3e6a6f6d478
Hana <120221238+Hana-Guta@users.noreply.github.com>
2023-12-21 22:50:20 +0300

    Added solution for Increasing Triplet Subsequence

--- onboarding/increasing-triplet-subsequence.py
+++ onboarding/increasing-triplet-subsequence.py
@@ -5,14 +5,17 @@ class Solution:
         sec_min_val = 2**31 - 1
         
         for num in nums:
-            if num <= min_val:
-                min_val = num
-            elif num <= sec_min_val:
-                sec_min_val = num
-            else:
+            if num > sec_min_val:
                 return True
-        
+            if num < min_val:
+                min_val = num
+            if num > min_val:
+                sec_min_val = num 
+                
         return False
+
+        
+    
 
             
 
