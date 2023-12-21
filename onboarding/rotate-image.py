0907f92f203f63704eaff04a20d4f60617c58801
Hana <120221238+Hana-Guta@users.noreply.github.com>
2023-12-21 22:19:47 +0300

    Added solution for Rotate Image

--- onboarding/rotate-image.py
+++ onboarding/rotate-image.py
@@ -1,12 +1,10 @@
-from typing import List
-
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
         N = len(matrix)
-        
+
         for row in range(N):
             for col in range(row, N):
                 matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
