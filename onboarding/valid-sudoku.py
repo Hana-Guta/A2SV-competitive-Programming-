60ea622cb890d49cca744df324c2f6eda68b8a6b
Hana <120221238+Hana-Guta@users.noreply.github.com>
2023-12-24 20:33:09 +0300

    Added solution for Valid Sudoku

---
+++ onboarding/valid-sudoku.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for row in board:
+            if not self.is_valid_unit(row):
+                return False
+
+        for col in zip(*board):
+            if not self.is_valid_unit(col):
+                return False
+
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                sub_box = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+                if not self.is_valid_unit(sub_box):
+                    return False
+
+        return True
+
+    def is_valid_unit(self, unit: List[str]) -> bool:
+        seen = set()
+        for cell in unit:
+            if cell != ".":
+                if cell in seen:
+                    return False
+                seen.add(cell)
+        return True
