b941451ac736f7cba3ff4634ea6bce53f75527c9
Hana <120221238+Hana-Guta@users.noreply.github.com>
2023-12-26 13:05:45 +0300

    Added solution for Number of Times Binary String Is Prefix-Aligned

---
+++ onboarding/number-of-times-binary-string-is-prefix-aligned.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numTimesAllBlue(self, flips: List[int]) -> int:
+        one = curr_max = 0
+        res = 0
+
+        for i in flips:
+            one += 1
+            curr_max = max(curr_max , i)
+            
+            if one == curr_max:
+                res += 1
+        return res
+
+   
+    
+      
+
+        
